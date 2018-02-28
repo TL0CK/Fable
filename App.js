e1dfc6db92a102ecda72d3cbf8ce047082c33946
TL0CK <appartement130@gmail.com>
2018-02-28 13:50:23 +0100

    Projet

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>La bite du cul</Text>
+        <Text>Fable</Text>
       </View>
     );
   }
