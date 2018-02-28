edea98dc02d7907ceaf004c9a2fe6d7cb46c5044
TL0CK <appartement130@gmail.com>
2018-02-28 14:46:18 +0100

    Routing

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+import HomeScreen from './pages/homePage';
+import DetailsScreen from './pages/detailsScreen';
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Fable</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
 
